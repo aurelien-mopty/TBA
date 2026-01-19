23187550975a8b2be4f9dd99315a9cf5eae4ef00
Gabriel-Lecoeur <gabriel.lecoeur@edu.esiee.fr>
2026-01-19 16:36:58 +0000

    item.py

--- item.py
+++ item.py
@@ -1,15 +1,25 @@
 class Item:
-
-   # Define the constructor. 
+    """
+    Module définissant la classe Item.
+    Ce module permet de gérer les objets transportables et leurs caractéristiques.
+    """
     def __init__(self, name, description, weight : float):
+        """
+        Représente un objet interactif dans le jeu.
+
+        Attributes:
+            name (str): Le nom de l'objet.
+            description (str): Une courte description de l'objet.
+            weight (float): Le poids de l'objet en kilogrammes.
+        """
         self.name = name
         self.description = description
         self.weight = weight
         self.charged_room = None
         self.is_beamer = False
-        
-        
-    def __str__(self):
-        return  f"{self.name} : {self.description} ({self.weight}kg)"
 
-    
+    def __str__(self):
+        """
+        Retourne une représentation textuelle de l'objet.
+        """
+        return  f"{self.name} : {self.description} ({self.weight}kg)"
