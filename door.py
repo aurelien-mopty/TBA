444ded6e4f7a66bf152ee6d0bc8e1922c158fcc4
Gabriel-Lecoeur <gabriel.lecoeur@edu.esiee.fr>
2026-01-19 16:13:19 +0000

    Modif door.py

--- door.py
+++ door.py
@@ -1,5 +1,16 @@
 class Door:
+    """
+    Représente une porte connectant une salle dans une direction donnée.
+    """
     def __init__(self, room, direction, locked=False):
+        """
+        Initialise une nouvelle porte.
+
+        :param room: La salle de destination.
+        :param direction: La direction de la porte (ex: 'nord').
+        :param locked: État de verrouillage de la porte.
+        """
+
         self.room = room 
         self.direction = direction  
-        self.locked = locked 
+        self.locked = locked
