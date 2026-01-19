5d0a307562db6a0a36ad19f6f298dc8ffbcd3cbe
Aurélien <aurelien.mopty@edu.esiee.fr>
2026-01-19 16:21:15 +0000

    docstring door.py

--- door.py
+++ door.py
@@ -1,3 +1,7 @@
+"""
+Module contenant la classe Door.
+"""
+
 class Door:
     """
     Représente une porte connectant une salle dans une direction donnée.
@@ -6,11 +10,12 @@ class Door:
         """
         Initialise une nouvelle porte.
 
-        :param room: La salle de destination.
-        :param direction: La direction de la porte (ex: 'nord').
-        :param locked: État de verrouillage de la porte.
+        Args:
+            room (Room): La salle de destination.
+            direction (str): La direction de la porte (ex: 'N').
+            locked (bool, optional): État de verrouillage de la porte. Defaults to False.
         """
 
-        self.room = room 
-        self.direction = direction  
+        self.room = room
+        self.direction = direction
         self.locked = locked
